d00a69255d1bae788a548d0cbc4ac1e86354ac99
dvikster <dvikster@gmail.com>
2017-05-11 13:51:47 +0300

    fixed

--- js.js
+++ js.js
@@ -2,20 +2,18 @@
  * Created by OSS PC 6 on 5/10/2017.
  */
 //Строка
-var str='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nulla eu lorem iaculis sagittis vel ut dui. Phasellus sagittis mauris id faucibus pulvinar.';
+var str='Lorem ipsum dolor L';
+// var str='pul';
+console.log(str);
 
 //Строка как массив.
-//Разбивка по символам
-var resSplitAll=str.split('');
-console.log(resSplitAll);
-
-//Разбивка по пробелам
-var resSplit=str.split(' ');
-console.log(resSplit);
-
-//Разбивка по предложениям
-var resSplitDot=str.split('.');
-console.log(resSplitDot);
+// Доступ к символам как к массиву
+// Чтобы получить символ, используйте вызов charAt(позиция).
+// Первый символ имеет позицию
+var strChartAt = str.charAt(0);
+var strChartAt2 = str[0];
+console.log(strChartAt);
+console.log(strChartAt2);
 
 //Подсчет символов в строке - Длина строки
 var strLength = str.length;
@@ -40,10 +38,10 @@ console.log(resAllLength);
 
 
 document.getElementById('str').innerHTML = str;
+document.getElementById('strChartAt').innerHTML = strChartAt;
+document.getElementById('strChartAt2').innerHTML = strChartAt2;
 document.getElementById('strLength').innerHTML = strLength;
-document.getElementById('resSplit').innerHTML = resSplit;
-document.getElementById('resSplitAll').innerHTML = resSplitAll;
-document.getElementById('resSplitDot').innerHTML = resSplitDot;
+
 document.getElementById('res').innerHTML = res;
 document.getElementById('resLength').innerHTML = resLength;
 document.getElementById('resAll').innerHTML = resAll;
@@ -51,4 +49,18 @@ document.getElementById('resAllLength').innerHTML = resAllLength;
 
 
 
+//Разбивка по символам
+// var resSplitAll=str.split('');
+// console.log(resSplitAll);
 
+//Разбивка по пробелам
+// var resSplit=str.split(' ');
+// console.log(resSplit);
+
+//Разбивка по предложениям
+// var resSplitDot=str.split('.');
+// console.log(resSplitDot);
+
+// document.getElementById('resSplit').innerHTML = resSplit;
+// document.getElementById('resSplitAll').innerHTML = resSplitAll;
+// document.getElementById('resSplitDot').innerHTML = resSplitDot;
